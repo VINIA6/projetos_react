98a357c8deabcadd3682f33884bf0eca422ee890
VINIA6 <viniciusdeassisazevedo@hotmail.com>
2023-06-26 16:16:18 -0300

    Array Loop no React

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,42 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
   state = {
-    nome: "Vini",
-    counter:0,
+    posts: [
+      {
+        id:1,
+        title:'O titulo 1',
+        body:'O corpo 1'
+      },
+      {
+        id:2,
+        title:'O titulo 2',
+        body:'O corpo 2'
+      },
+      {
+        id:3,
+        title:'O titulo 3',
+        body:'O corpo 3'
+      },
+    ],
   };
   
-  handlePclick = () => {
-    this.setState({nome:'Lúcia'});
-  }
-
-  handleAclick = (event) => {
-    event.preventDefault();
-    let {counter} = this.state;
-    this.setState({counter: counter + 1});
-  }
-
   render() {
-    const {nome, counter} = this.state;
+
+    const {posts} = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePclick}>
-            {nome} {counter}
-          </p>
-          <a
-          onClick={this.handleAclick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          posts.map(post => 
+            <div key={post.id}>
+              <h1 key={post.id}> {post.title} </h1>
+              <p>{post.body}</p>
+            </div>
+          )
+        }
       </div>
     );
   }
